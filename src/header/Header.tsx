987c2c6e362b8c35c24fbcb096bdd2a0531a224d
Freezerq <thepazdk@gmail.com>
2023-02-06 22:14:35 +0600

    good css

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import classes from './Header.module.css'
 import Nav from "../nav/Nav";
-
+import container from "../common/styles/Container.module.css";
+// className={`${container.main} ${classes.projectsContainer}`}
+//        <header className={classes.header}>
 const Header = () => {
     return (
-        <header className={classes.header}>
-            <Nav/>
-        </header>
+        <div className={classes.main}>
+            <header className={`${container.main} ${classes.headerContainer}`}>
+                <Nav/>
+            </header>
+        </div>
     );
 };
 
